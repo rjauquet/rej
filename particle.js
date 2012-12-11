2c92ef4753f8b5f4f9efe9bbbc9a3eeeea6de1c6
Rob <rjauquet@gmail.com>
2012-12-11 15:46:34 -0600

    added timing, removed redundant calls

--- particle.js
+++ particle.js
@@ -1,9 +1,8 @@
 var canvas, ctx, center, cur, next, speed, num;
 
-function particle(x,y,mass,rad,r,g,b){
+function particle(x,y,rad,r,g,b){
     this.x=x;
     this.y=y;
-    this.mass=mass;
     this.rad=rad;
     this.r=r;
     this.g=g;
@@ -16,45 +15,29 @@ function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     num = 100;
-    cur = getParticles(num);
-}
-        
-function run(){
-    next = getParticles(num); 
-    speed = 1000;        
-    motion();
-    delete next;
+    speed = 50; 
+    cur = getParticles(num).slice(0);
+    next = getParticles(num).slice(0);
+    setInterval(update,speed);
 }
         
 function getParticles(num){
     var particles = new Array();
+    var radMax = 40;
+    var radMin = 5;
     for(var i=0; i<num; i++){
         var x = Math.floor((Math.random()*canvas.width)+1);
         var y = Math.floor((Math.random()*canvas.height)+1);
-        var mass = Math.floor((Math.random()*100)+25);
-        var rad = Math.floor((Math.random()*15)+1);
+        var rad = Math.floor((Math.random()*(radMax-radMin))+radMin);
         var rgb = getRGB(50,0,255,125);
-        particles.push(new particle(x,y,mass,rad,rgb.r,rgb.g,rgb.b));    
+        particles.push(new particle(x,y,rad,rgb.r,rgb.g,rgb.b));                
     }
     return particles;        
 }
 
-function motion(){
-    var start = cur;
-    for(var j=0; j<speed; j++){
-        var interval = setInterval(function() { 
-            update(start);
-            if(j == speed-1){ 
-                clearInterval(interval)
-            }
-         }, 1000);
-    }
-}
-
-function update(start){
-    DrawCanvas(cur); 
+function update(){
     for(var i=0; i<cur.length; i++){
-        cur[i].x += (next[i].x-start[i].x)/(i+1);
+        cur[i].x += (next[i].x-cur[i].x)/speed;
         if(cur[i].x < 0){
             cur[i].x = 0;
         }
@@ -62,7 +45,7 @@ function update(start){
             cur[i].x = canvas.width - 1;
         }
         
-        cur[i].y += (next[i].y-start[i].y)/(i+1);
+        cur[i].y += (next[i].y-cur[i].y)/speed;
         if(cur[i].y < 0){
             cur[i].y = 0;
         }
@@ -70,14 +53,14 @@ function update(start){
             cur[i].y= canvas.height - 1;
         }
         
-        cur[i].rad += Math.floor((next[i].rad-start[i].rad)/(i+1));
-        if(cur[i].rad < 0){
-            cur[i].rad = 1;
+        cur[i].rad += (next[i].rad-cur[i].rad)/speed;
+        if(cur[i].rad < 3){
+            cur[i].rad = 3;
         }
-        else if(cur[i].rad > 15){
-            cur[i].rad = 15;
+        else if(cur[i].rad > 25){
+            cur[i].rad = 25;
         }
-        cur[i].r += Math.floor((next[i].r-start[i].r)/(i+1));
+        cur[i].r += Math.floor((next[i].r-cur[i].r)/speed);
         if(cur[i].r < 0){
             cur[i].r = 0;
         }
@@ -85,7 +68,7 @@ function update(start){
             cur[i].r = 255;
         }
         
-        cur[i].g += Math.floor((next[i].g-start[i].g)/(i+1));
+        cur[i].g += Math.floor((next[i].g-cur[i].g)/speed);
         if(cur[i].g < 0){
             cur[i].g = 0;
         }
@@ -93,89 +76,97 @@ function update(start){
             cur[i].g = 255;
         }
         
-        cur[i].b += Math.floor((next[i].b-start[i].b)/(i+1));
+        cur[i].b += Math.floor((next[i].b-cur[i].b)/speed);
         if(cur[i].b < 0){
             cur[i].b = 0;
         }
         else if(cur[i].b > 255){
             cur[i].b = 255;
         }
+        DrawCanvas();
+    }
+    if((Math.abs(cur[0].x - next[0].x) < 2) && (Math.abs(cur[0].y - next[0].y) < 2)){
+        console.log("here")
+        cur = next.slice(0);
+        next = getParticles(num).slice(0);
     }
 }
 
 // draw canvas
 
-function DrawCanvas(particles) {
+function DrawCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.lineWidth = 5;
 
-    for(var i=0; i<particles.length; i++){
-        ctx.strokeStyle = "rgb("+particles[i].r+","+particles[i].g+","+particles[i].b+")";
-        ctx.fillStyle = "rgb("+particles[i].r+","+particles[i].g+","+particles[i].b+")";
+    for(var i=0; i<cur.length; i++){
+        ctx.strokeStyle = "rgb("+cur[i].r+","+cur[i].g+","+cur[i].b+")";
+        ctx.fillStyle = "rgb("+cur[i].r+","+cur[i].g+","+cur[i].b+")";
         ctx.beginPath();
-        ctx.moveTo(particles[i].x, particles[i].y);
-        ctx.arc(particles[i].x, particles[i].y, particles[i].rad, 0, 2*Math.PI, true);
+        ctx.moveTo(cur[i].x, cur[i].y);
+        ctx.arc(cur[i].x, cur[i].y, cur[i].rad, 0, 2*Math.PI, true);
         ctx.fill();
         ctx.stroke();
     }
      
     ctx.closePath();
 }
-
+//getRGB(50,0,255,125);
 function getRGB(red,green,blue,range){
-    var rp,rm,gp,gm,bp,bm;
+    var rhigh,rlow,ghigh,glow,bhigh,blow,rrand,grand,brand;
     
     //set red upper
     if((red+range) > 255){
-        rp = 255;       
+        rhigh = 255;       
     }
     else{
-        rp = red + range;
+        rhigh = red + range;
     }
     
     //set red lover
     if((red-range) < 0){
-        rm = 0;       
+        rlow = 0;       
     }
     else{
-        rm = red - range;
+        rlow = red - range;
     }
     
     //set green upper
     if((green+range) > 255){
-        gp = 255;       
+        ghigh = 255;       
     }
     else{
-        gp = green + range;
+        ghigh = green + range;
     }
     
     //set green lower
     if((green-range) < 0){
-        gm = 0;       
+        glow = 0;       
     }
     else{
-        gm = green - range;
+        glow = green - range;
     }
     
     //set blue upper
     if((blue+range) > 255){
-        bp = 255;       
+        bhigh = 255;       
     }
     else{
-        bp = blue + range;
+        bhigh = blue + range;
     }
     
     //set blue lower
     if((blue-range) < 0){
-        bm = 0;       
+        blow = 0;       
     }
     else{
-        bm = blue - range;
+        blow = blue - range;
     }
-    
+    rrand = Math.floor((Math.random()*(rhigh - rlow))+rlow);
+    grand = Math.floor((Math.random()*(ghigh - glow))+glow);
+    brand = Math.floor((Math.random()*(bhigh - blow))+blow);
     return {        
-        r: Math.floor((Math.random()*rp)+rm),
-        g: Math.floor((Math.random()*gp)+gm),
-        b: Math.floor((Math.random()*bp)+bm) 
+        r: rrand,
+        g: grand,
+        b: brand 
     };
 }​
