05ff8dda71206ec4d7de0b027a226e99c83bc660
Rob <rjauquet@gmail.com>
2012-12-12 15:39:56 -0600

    added beginnings of English Draughts

--- particle.js
+++ particle.js
@@ -1,4 +1,4 @@
-var canvas, ctx, center, cur, next, speed, num;
+var canvas, ctx, center, cur, next, speed, num, p1, p2, spaces, check, checkrad, rgbp1, rgbp2, drag=null,dPoint, boardx, boardy;
 
 function particle(x,y,rad,r,g,b){
     this.x=x;
@@ -12,18 +12,60 @@ function particle(x,y,rad,r,g,b){
 
 function init() {
     // start
-    canvas = document.getElementById("canvas");
+    canvas = document.getElementById("background_canvas");
     ctx = canvas.getContext("2d");
     canvas.width = $(window).width();
     canvas.height = $(window).height();
     
+    check = 15;
+    checkrad = 6;
     num = 100;
     speed = 50; 
+    boardx = canvas.width/6+15;
+    boardy = canvas.height-(8*15+10);
     cur = getParticles(num).slice(0);
     next = getParticles(num).slice(0);
+    rgbp1 = getRGB(255,255,255,100);
+    rgbp2 = getRGB(0,0,0,100);
+    
+    canvas.addEventListener("mousedown", DragStart, false);
+    canvas.addEventListener("mousemove", Dragging, false);
+    document.body.addEventListener("mouseup", DragEnd, false);
+    
+    setUpDraughts();
+    
     setInterval(update,speed);
 }
-        
+
+function setUpDraughts(){
+    p1 = new Array();
+    p2 = new Array();
+    spaces = new Array();
+    var num = 0;
+    for (var i=0;i<32; i++){
+        spaces.push({
+            x:0,
+            y:0,
+            state: 0
+        });
+    }
+    for(var row=0; row<3; row++){
+        for(var i=0; i<4; i++){
+            p1.push({
+                x:2*i*check+(check*(row%2)),
+                y:row*check,
+                s:num        
+            });
+            p2.push({
+                x:2*i*check+(check*((row+1)%2)), 
+                y:(5+row)*check,
+                s:20+num               
+            });
+            num++;
+        }
+    }
+}     
+
 function getParticles(num){
     var particles = new Array();
     var radMax = 40;
@@ -41,14 +83,16 @@ function getParticles(num){
 function update(){
     var toNext = 0;
     for(var i=0; i<cur.length; i++){
-        var dx = (next[i].x-cur[i].x)/speed;
-        var dy = (next[i].y-cur[i].y)/speed;
-        var drad = (next[i].rad-cur[i].rad)/speed;
-        var dr = Math.floor((next[i].r-cur[i].r)/speed);
-        var dg = Math.floor((next[i].g-cur[i].g)/speed);
-        var db = Math.floor((next[i].b-cur[i].b)/speed);
+        if(toNext == 0){
+            cur[i].dx = (next[i].x-cur[i].x)/speed;
+            cur[i].dy = (next[i].y-cur[i].y)/speed;
+            cur[i].drad = (next[i].rad-cur[i].rad)/speed;
+            cur[i].dr = Math.floor((next[i].r-cur[i].r)/speed);
+            cur[i].dg = Math.floor((next[i].g-cur[i].g)/speed);
+            cur[i].db = Math.floor((next[i].b-cur[i].b)/speed);
+        }
         
-        cur[i].x += dx;
+        cur[i].x += cur[i].dx;
         if(cur[i].x < 0){
             cur[i].x = 0;
         }
@@ -56,7 +100,7 @@ function update(){
             cur[i].x = canvas.width - 1;
         }
         
-        cur[i].y += dy;
+        cur[i].y += cur[i].dy;
         if(cur[i].y < 0){
             cur[i].y = 0;
         }
@@ -64,14 +108,14 @@ function update(){
             cur[i].y= canvas.height - 1;
         }
         
-        cur[i].rad += drad
+        cur[i].rad += cur[i].drad
         if(cur[i].rad < 3){
             cur[i].rad = 3;
         }
         else if(cur[i].rad > 25){
             cur[i].rad = 25;
         }
-        cur[i].r += dr;
+        cur[i].r += cur[i].dr;
         if(cur[i].r < 0){
             cur[i].r = 0;
         }
@@ -79,7 +123,7 @@ function update(){
             cur[i].r = 255;
         }
         
-        cur[i].g += dg
+        cur[i].g += cur[i].dg;
         if(cur[i].g < 0){
             cur[i].g = 0;
         }
@@ -87,7 +131,7 @@ function update(){
             cur[i].g = 255;
         }
         
-        cur[i].b += db;
+        cur[i].b += cur[i].db;
         if(cur[i].b < 0){
             cur[i].b = 0;
         }
@@ -97,7 +141,7 @@ function update(){
         canvas.width = $(window).width();
         canvas.height = $(window).height();
         DrawCanvas();
-        if(dx < .07 && dy < .07){
+        if( Math.abs(cur[i].x - next[i].x) < .5 && Math.abs(cur[i].y - next[i].y) < .5){
             toNext++;            
         }
     }
@@ -113,6 +157,8 @@ function update(){
 
 function DrawCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
     ctx.lineWidth = 5;
 
     for(var i=0; i<cur.length; i++){
@@ -124,8 +170,82 @@ function DrawCanvas() {
         ctx.fill();
         ctx.stroke();
     }
-     
+    
+    ctx.fillStyle = 'black';
+    ctx.fillRect(canvas.width/6,0,canvas.width*2/3,canvas.height);
+    boardx = canvas.width/6+15;
+    boardy = canvas.height-(8*15+10);
+    drawText(canvas.width/6,0,canvas.width*2/3);
+    drawEnglishDraughts(boardx,boardy);
     ctx.closePath();
+}
+
+function drawText(startx,starty,width){
+    ctx.font = '14px Calibri';
+    ctx.fillStyle = 'white';
+    var text = "Hello. My name is Rob Jauquet. The purpose of this site is to give a representation of my background using javascript, as well as showcase my experience animating on HTML5's canvas.";
+    wrapText(text, startx+5, starty+28, width-5, 16);
+}
+  
+function drawEnglishDraughts(startx,starty){
+    var black = false;
+    var num = 0;
+    for(var i=0; i<8; i++){
+        for(var j=0; j<8; j++){
+            if(black){
+                ctx.fillStyle = 'black';
+                black = false;
+            }
+            else{
+                ctx.fillStyle = 'white';
+                black = true;
+                spaces[num].x = i*15;
+                spaces[num].y = j*15;
+                num++;
+            }
+            ctx.fillRect(startx+i*15,starty+j*15,15,15);
+        }
+        black = !black;           
+    }
+    drawDraughts(startx,starty);
+}
+function drawDraughts(startx,starty){
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "rgb("+rgbp2.r+","+rgbp2.g+","+rgbp2.b+")";
+    ctx.fillStyle = "rgb("+rgbp1.r+","+rgbp1.g+","+rgbp1.b+")";
+    for(var i=0; i<p1.length; i++){
+        ctx.beginPath();
+        ctx.arc(startx+p1[i].x+check/2,starty+p1[i].y+check/2, checkrad, 0, 2*Math.PI, true);
+        ctx.fill();
+        ctx.stroke();       
+    }
+    ctx.strokeStyle = "rgb("+rgbp1.r+","+rgbp1.g+","+rgbp1.b+")";
+    ctx.fillStyle = "rgb("+rgbp2.r+","+rgbp2.g+","+rgbp2.b+")";
+    for(var i=0; i<p2.length; i++){
+        ctx.beginPath();
+        ctx.arc(startx+p2[i].x+check/2,starty+p2[i].y+check/2, checkrad, 0, 2*Math.PI, true);
+        ctx.fill();
+        ctx.stroke();
+    }
+}  
+function wrapText(text, x, y, maxWidth, lineHeight) {
+    var words = text.split(' ');
+    var line = '';
+    
+    for(var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + ' ';
+        var metrics = ctx.measureText(testLine);
+        var testWidth = metrics.width;
+        if(testWidth > maxWidth) {
+            ctx.fillText(line, x, y);
+            line = words[n] + ' ';
+            y += lineHeight;
+        }
+        else {
+            line = testLine;
+        }
+    }
+    ctx.fillText(line, x, y);
 }
 //getRGB(50,0,255,125);
 function getRGB(red,green,blue,range){
@@ -186,4 +306,76 @@ function getRGB(red,green,blue,range){
         g: grand,
         b: brand 
     };
+}
+    
+function DragStart(e){
+    e = MousePos(e);
+    var dx,dy;
+    for(var i=0; i<p1.length; i++){
+        dx = e.x - (p1[i].x + boardx + checkrad);
+        dy = e.y - (p1[i].y + boardy + checkrad);       
+        if((dx*dx + dy*dy) < checkrad*checkrad){
+            drag = i;
+            dPoint = e;
+            canvas.style.cursor = "none";
+            return;
+        }       
+    }
+}
+    
+function Dragging(e){
+    if (drag != null) {
+        e = MousePos(e);
+        p1[drag].x += e.x - dPoint.x;
+        p1[drag].y += e.y - dPoint.y;
+        dPoint = e;
+        DrawCanvas();
+    } 
+}
+    
+function DragEnd(e){
+    if(drag != null){
+        var closest=null,cur,closest = 1000, index = p1[drag].s;
+        for(var i=0; i<spaces.length; i++){
+              cur = Math.sqrt((p1[drag].x - spaces[i].x)*(p1[drag].x - spaces[i].x) + (p1[drag].y - spaces[i].y)*(p1[drag].y - spaces[i].y));
+            if(cur < closest){
+                closest = cur;
+                index = i;               
+            }
+        }
+            
+        p1[drag].x = spaces[index].x;
+        p1[drag].y = spaces[index].y;
+        p1[drag].s = index;
+        spaces[index].state = 1;
+        drag = null;
+        canvas.style.cursor = "default";
+        DrawCanvas();
+    }
+}
+        
+function MousePos(event) {
+    event = (event ? event : window.event); 
+    var pos = findPos(canvas);
+    var x = event.pageX - pos[0];
+    var y = event.pageY - pos[1];
+    return {
+        x: x,
+        y: y
+    }
+}
+        
+function findPos(e){
+    var offs = new Array()
+    var curleft = 0, curtop = 0;
+    if (e.offsetParent) {
+        do {
+            curleft += e.offsetLeft;
+            curtop += e.offsetTop;
+        } while (e = e.offsetParent);
+        offs.push(curleft);
+        offs.push(curtop);
+        return offs;
+    }
+    return undefined;
 }​
